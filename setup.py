efc18fef2e1c995ac9f0abba215cbed020b28c11
hsolbrig <solbrig.harold@mayo.edu>
2016-11-29 15:11:37 -0500

    Updates version for pypi

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ if sys.version_info < (3, 5):
 
 setup(
     name='SNOMEDToOWL',
-    version='0.0.4',
+    version='0.0.5',
     packages=['SNOMEDCTToOWL', 'SNOMEDCTToOWL.RF2Files'],
     url='http://github.com/hsolbrig/SNOMEDToOWL',
     license='Apache License 2.0',
