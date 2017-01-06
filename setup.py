ee57052a6749459e2702b36a0341e03d6b5e448a
hsolbrig <solbrig.harold@mayo.edu>
2017-01-05 18:32:57 -0600

    Change version number for new pypi image

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ if sys.version_info < (3, 5):
 
 setup(
     name='SNOMEDToOWL',
-    version='0.2.2',
+    version='0.2.3',
     packages=['SNOMEDCTToOWL', 'SNOMEDCTToOWL.RF2Files'],
     package_data={'SNOMEDCTToOWL' : ['conf/*.json']},
     url='http://github.com/hsolbrig/SNOMEDToOWL',
